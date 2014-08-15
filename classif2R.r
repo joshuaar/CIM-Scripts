3d37508f2e7afac80b68f9ab71a65829a4d89df1
Josh Richer <joshamosricher@gmail.com>
2014-08-15 15:22:25 -0700

    classif mods

--- classif2R.r
+++ classif2R.r
@@ -33,10 +33,21 @@ crossValIteration = function(data, classes, p, p.adjust=F, shuffle=F){
 	# classes: a length m vector of class labels
 	# p: P value cutoff
 	# p.adjust: adjust p value method. 
-		FALSE: Do not do p adjustment
-		"BH": Benjamini and Hochberg adjustment
-		check documentation for p.adjust for more methods
+	#	FALSE: Do not do p adjustment
+	#	"BH": Benjamini and Hochberg adjustment
+	#	check documentation for p.adjust for more methods
 	# shuffle: Shuffle the class labels randomly without replation
-	splittedData = trainTestSplit(data,classes)
-	#incomplete
+	splittedData = trainTestSplit(data,classes)# list with train_data, test_data, train_classes, test_classes fields
+	
+	XTrain = splittedData$train_data	
+	XTest = splittedData$test_data	
+	YTrain = as.factor(splittedData$train_classes)
+	YTest = as.factor(splittedData$test_classes)	
+	
+	YTrain.model = model.matrix(~YTrain)
+	YTrain.model0 = model.matrix(~1,data=YTrain)
+	trainSv = sva(XTrain,YTrain.model,YTrain.model0)
+	
+	fsvaobj = fsva(dbdat=XTrain, mod=YTrain.model, sv=trainSv, newdat=XTest)
+	XTrain_adjusted = fsvaobj$db
 }	
